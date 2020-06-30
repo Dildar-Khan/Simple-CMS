14790823acdd291adf26d1aaece164c9cb3822a7
Dildar-Khan <dildarzbs@gmail.com>
2020-06-30 14:17:30 +0500

    Deployed & Working

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,55 +3,59 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.createUser = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10).
-  then(hash => {
+  bcrypt.hash(req.body.password, 10).then((hash) => {
     const user = new User({
       email: req.body.email,
-      password: hash
+      password: hash,
     });
-    user.save().then(result => {
-      res.status(201).json({
-        message: 'User created!',
-        result: result
-      });
-    }).catch(err => {
-      res.status(500).json({
-          message: 'E-mail is already registered!'
+    user
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: 'User created!',
+          result: result,
+        });
       })
-    })
-  })
-  }
+      .catch((err) => {
+        res.status(500).json({
+          message: 'E-mail is already registered!',
+        });
+      });
+  });
+};
 
-  exports.userLogin = (req, res, next) => {
-    let fetchedUser;
-    User.findOne({ email: req.body.email })
-    .then(user => {
+exports.userLogin = (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email })
+    .then((user) => {
       if (!user) {
         return res.status(401).json({
-          message: 'User not existed'
+          message: 'User not existed',
         });
       }
       fetchedUser = user;
       return bcrypt.compare(req.body.password, user.password);
     })
-    .then(result => {
+    .then((result) => {
       if (!result) {
         return res.status(401).json({
-          message: 'User not existed'
+          message: 'User not existed',
         });
       }
       const token = jwt.sign(
-        {email: fetchedUser.email, userId: fetchedUser._id},
-        process.env.JWT_KEY, {expiresIn: '1h'});
-      res.status(200).json({
+        { email: fetchedUser.email, userId: fetchedUser._id },
+        'za_mara_ta_ta_da_wali_gap_khkari_sa',
+        { expiresIn: '1h' }
+      );
+      return res.status(200).json({
         token: token,
         expiresIn: 3600,
-        userId: fetchedUser._id
-      })
+        userId: fetchedUser._id,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       return res.status(401).json({
-        message: 'Invalid E-mail or password, please try again!'
+        message: 'Invalid E-mail or password, please try again!',
       });
     });
-  }
+};
